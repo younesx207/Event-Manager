f0174d9594ce6fa57bd9cbed59d51ade08e5f156
YounesKBE <122362381+younesx207@users.noreply.github.com>
2023-10-18 11:19:25 +0200

    new changes

---
+++ configs/firebase_config_example.py
@@ -0,0 +1,8 @@
+firebaseConfig = {
+  "apiKey": "AIzaSyAh0KbIe1PdHQT2USL8PLVGNYquDK_suKA",
+  "authDomain": "event-calendar-27b62.firebaseapp.com",
+  "projectId": "event-calendar-27b62",
+  "storageBucket": "event-calendar-27b62.appspot.com",
+  "messagingSenderId": "747564769165",
+  "appId": "1:747564769165:web:a21c588ba1103839dea7c4"
+}
---
+++ database/firebase.py
@@ -0,0 +1,15 @@
+import firebase_admin
+from firebase_admin import credentials
+from configs.firebase_config_example.py import firebaseConfig
+import pyrebase
+
+if not firebase_admin._apps:
+
+    cred = credentials.Certificate("event-calendar-27b62-firebase-adminsdk-6bd1x-c7695f8512.json")
+
+    firebase_admin.initialize_app(cred)
+
+#cred = credentials.Certificate("path/to/serviceAccountKey.json")
+#firebase_admin.initialize_app(cred)
+
+firebase = pyrebase.initialize_app(firebaseConfig)
